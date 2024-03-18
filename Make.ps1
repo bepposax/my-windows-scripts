16e94aacf8045c330c0cd87fa79673d887cb44f9
bepposax <bepposax@hotmail.it>
2024-03-18 17:21:40 +0100

    Copy multiple files

--- Make.ps1
+++ Make.ps1
@@ -11,13 +11,8 @@ New-Item -Path "$Documents\" -Name "AutoHotkey" -ItemType "directory"
 Copy-Item -Path ".\tilde.ahk" -Destination "$Documents\AutoHotkey"
 
 New-Item -Path "$workspace\" -Name "Powershell Scripts" -ItemType "directory"
-Copy-Item -Path ".\Clear Temp Folder.ps1" -Destination "$workspace\Powershell Scripts"
-Copy-Item -Path ".\Battery Report.ps1" -Destination "$workspace\Powershell Scripts"
-Copy-Item -Path ".\Energy Report.ps1" -Destination "$workspace\Powershell Scripts"
-
-Copy-Item -Path ".\Clear Temp Folder.ps1.lnk" -Destination "%home\Desktop"
-Copy-Item -Path ".\Battery Report.ps1.lnk" -Destination "%home\Desktop"
-Copy-Item -Path ".\Energy Report.ps1.lnk" -Destination "%home\Desktop"
+Copy-Item -Path ".\*.ps1" -Destination "$workspace\Powershell Scripts"
+Copy-Item -Path ".\*.lnk" -Destination "$home\Desktop"
 
 cd ..
 Remove-Item "my-windows-scripts" -Recurse
