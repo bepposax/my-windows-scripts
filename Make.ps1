86b5c7f374654c233bae57e4dc35a3750caf5605
bepposax <bepposax@hotmail.it>
2024-03-18 17:28:04 +0100

    avoid copying make.ps1

--- Make.ps1
+++ Make.ps1
@@ -8,11 +8,11 @@ $Documents = "$drv\Documents"
 $workspace = "$drv\workspace"
 
 New-Item -Path "$Documents\" -Name "AutoHotkey" -ItemType "directory"
-Copy-Item -Path ".\tilde.ahk" -Destination "$Documents\AutoHotkey"
+Copy-Item -Path ".\scripts\tilde.ahk" -Destination "$Documents\AutoHotkey"
 
 New-Item -Path "$workspace\" -Name "Powershell Scripts" -ItemType "directory"
-Copy-Item -Path ".\*.ps1" -Destination "$workspace\Powershell Scripts"
-Copy-Item -Path ".\*.lnk" -Destination "$home\Desktop"
+Copy-Item -Path ".\scripts\*.ps1" -Destination "$workspace\Powershell Scripts"
+Copy-Item -Path ".\links\*" -Destination "$home\Desktop"
 
 cd ..
 Remove-Item "my-windows-scripts" -Recurse
