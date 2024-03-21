b9ce150fddeabae8ee13ff51b9f59df949c9b7e4
bepposax <bepposax@hotmail.it>
2024-03-21 04:11:32 +0100

    Add laptop check
    If it's not a laptop I don't need battery and energy scripts

--- Make.ps1
+++ Make.ps1
@@ -1,3 +1,5 @@
+$IsLaptop = (Get-WmiObject -Class Win32_ComputerSystem -Property PCSystemType).PCSystemType -eq 2
+
 if (Test-Path -Path "E:") {
     $workspace = "E:\workspace"
     $Documents = "E:\Documents"
@@ -11,8 +13,12 @@ Copy-Item -Path ".\scripts\tilde.ahk" -Destination "$Documents\AutoHotkey"
 Copy-Item -Path ".\links\tilde.ahk.lnk" -Destination "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 
 New-Item -Path "$workspace\" -Name "Powershell Scripts" -ItemType "directory"
-Copy-Item -Path ".\scripts\*.ps1" -Destination "$workspace\Powershell Scripts"
-Copy-Item -Path ".\links\*.ps1.lnk" -Destination "$home\Desktop"
-
+if ($IsLaptop) {
+    Copy-Item -Path ".\scripts\*.ps1" -Destination "$workspace\Powershell Scripts"
+    Copy-Item -Path ".\links\*.ps1.lnk" -Destination "$home\Desktop"
+} else {
+    Copy-Item -Path ".\scripts\Clear Temp Folder.ps1" -Destination "$workspace\Powershell Scripts"
+    Copy-Item -Path ".\links\Clear Temp Folder.ps1.lnk" -Destination "$home\Desktop"
+}
 cd ..
 Remove-Item "my-windows-scripts" -Recurse
