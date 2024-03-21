b6932138d54e974626c101575b8d06c78a21ed33
bepposax <bepposax@hotmail.it>
2024-03-21 03:40:21 +0100

    Add `tilde.ahk` to startup folder

--- Make.ps1
+++ Make.ps1
@@ -8,6 +8,7 @@ if (Test-Path -Path "E:") {
 
 New-Item -Path "$Documents\" -Name "AutoHotkey" -ItemType "directory"
 Copy-Item -Path ".\scripts\tilde.ahk" -Destination "$Documents\AutoHotkey"
+Copy-Item -Path ".\links\tilde.ahk.lnk" -Destination "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 New-Item -Path "$workspace\" -Name "Powershell Scripts" -ItemType "directory"
 Copy-Item -Path ".\scripts\*.ps1" -Destination "$workspace\Powershell Scripts"
